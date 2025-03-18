9508f42949cca58749e09a504f5c67d464fe9ba9
Michał Kuś <s30367@pjwstk.edu.pl>
2025-03-18 21:50:34 +0100

    Add Temperature Dictionary

---
+++ Program3/TemperatureDictionary.cs
@@ -0,0 +1,19 @@
+namespace Program3;
+
+public class TemperatureDictionary
+{
+    public static Dictionary<string, double> Temperatures = new Dictionary<string, double>()
+    {
+        {"bananas", 13.3},
+        {"chocolate", 18},
+        {"fish", 2},
+        {"meat", -15},
+        {"ice cream",-18},
+        {"frozen pizza", -30},
+        {"cheese", 7.2},
+        {"sausages", 5},
+        {"butter", 20.5},
+        {"eggs", 19},
+        
+    };
+}
