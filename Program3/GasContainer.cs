4d03b2bf837b013f3e85a187ae80e6e4cb39ecc3
Michał Kuś <s30367@pjwstk.edu.pl>
2025-03-18 21:43:47 +0100

    Add HazardNotifier to GasContainer

--- Program3/GasContainer.cs
+++ Program3/GasContainer.cs
@@ -1,6 +1,6 @@
 namespace Program3;
 
-public class GasContainer : Container
+public class GasContainer : Container, IHazardNotifier
 { 
     public double Pressure { get; set; }
     
@@ -9,5 +9,14 @@ public class GasContainer : Container
         SerialNumber = "KON-G-"+SerialNumberCounter++;
         Pressure = pressure;
     }
-    
+
+    public override void UnloadCargo()
+    {
+        CargoMass = CargoMass * 0.05;
+    }
+
+    public void NotifyHazard()
+    {
+        Console.WriteLine($"Container {SerialNumber} is notifying hazard");
+    }
 }
