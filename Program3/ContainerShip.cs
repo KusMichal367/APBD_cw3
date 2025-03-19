6330efa398541f223c642c107dffcf042232af07
Michał Kuś <s30367@pjwstk.edu.pl>
2025-03-19 10:14:10 +0100

    Update ContainerShip.cs

--- Program3/ContainerShip.cs
+++ Program3/ContainerShip.cs
@@ -1,11 +1,22 @@
 namespace Program3;
 
-public class ContainerShip(int maxSpeed, int maxContainerCapacity, double maxContainerWeight)
+public class ContainerShip()
 {
     public List<Container> Containers { get; set; } = [];
-    public int MaxSpeed  { get; set; } = maxSpeed;
-    public int MaxContainerCapacity { get; set; } = maxContainerCapacity;
-    public double MaxContainerWeight { get; set; } = maxContainerWeight;
+    public int MaxSpeed  { get; set; }
+    public int MaxContainerCapacity { get; set; }
+    public double MaxContainerWeight { get; set; }
+    
+    public string ShipName { get; set; }
+    public static int ShipNameCounter = 1;
+
+    public ContainerShip(int maxSpeed, int maxContainerCapacity, double maxContainerWeight) : this()
+    {
+        MaxSpeed = maxSpeed;
+        MaxContainerCapacity = maxContainerCapacity;
+        MaxContainerWeight = maxContainerWeight;
+        ShipName = $"Ship{ShipNameCounter++}";
+    }
 
     public void LoadContainer(string serialNumber)
     {
