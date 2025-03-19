de787113c43f9a471c42fb25b31685116744cc58
Michał Kuś <s30367@pjwstk.edu.pl>
2025-03-19 16:40:40 +0100

    Update Container.cs

--- Program3/Container.cs
+++ Program3/Container.cs
@@ -1,15 +1,29 @@
 namespace Program3;
 
-public abstract class Container(int cargoMass, int height, int containerMass, int depth, int maxCargoMass)
+public abstract class Container
 {
-    public double CargoMass { get; set; } = cargoMass;
-    public int Height { get; set; } = height;
-    public int ContainerMass { get; set; } = containerMass;
-    public int Depth { get; set; } = depth;
+    public double CargoMass { get; set; }
+    public int Height { get; set; }
+    public int ContainerMass { get; set; }
+    public int Depth { get; set; }
 
     public string SerialNumber { get; set; }
     protected static int SerialNumberCounter = 1;
-    public double MaxCargoMass { get; set; } = maxCargoMass;
+    public double MaxCargoMass { get; set; }
+
+    protected Container(double cargoMass, int height, int containerMass, int depth, double maxCargoMass)
+    {
+        if (CargoMass>MaxCargoMass)
+        {
+            throw new OverflowException("Cargo mass cannot be greater than max cargo mass");
+        }
+        
+        CargoMass = cargoMass;
+        Height = height;
+        ContainerMass = containerMass;
+        Depth = depth;
+        MaxCargoMass = maxCargoMass;
+    }
 
     public virtual void UnloadCargo()
     {
@@ -27,7 +41,7 @@ public abstract class Container(int cargoMass, int height, int containerMass, in
 
     public override string ToString()
     {
-        return $"Container {SerialNumber} \n \t Cargo Mass: {cargoMass} \n \t Max Cargo Mass: {maxCargoMass}" +
-               $"\n \t Height: {height} \n \t Depth: {depth} \n \t container mass: {CargoMass}";
+        return $"Container {SerialNumber} \n \t Cargo Mass: {CargoMass} kg \n \t Max Cargo Mass: {MaxCargoMass} kg" +
+               $"\n \t Height: {Height} cm \n \t Depth: {Depth} cm\n \t container mass: {CargoMass}kg";
     }
 }
